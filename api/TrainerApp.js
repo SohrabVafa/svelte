6a284a7634ac79739b8b61307b42ce0817fb4a42
Sohrab Vafa <56256479+SohrabVafa@users.noreply.github.com>
2020-12-19 23:12:50 +0100

    Update TrainerApp.js

--- api/TrainerApp.js
+++ api/TrainerApp.js
@@ -1,12 +1,12 @@
 module.exports = (req, res) => {
-    let bodyString = req.body;
-    var entradaDialogFlow = JSON.parse(bodyString);
+//     let bodyString = req.body;
+//     var entradaDialogFlow = JSON.parse(bodyString);
     //console.log("Entrada DialogFlow: "+entradaDialogFlow);
 //     var nombreIntent = entradaDialogFlow['queryResult']['intent'].displayName;
 
     //console.log("Intent: "+nombreIntent);
 
-    var respuesta = "Hola";
+    let respuesta = "Hola";
 
     // if (nombreIntent == "Informacion_actividad") {
     //     var actividad = entradaDialogFlow['queryResult']['parameters'].actividad;

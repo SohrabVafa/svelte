8ec494cf9e4b0d4bdb789594ab44bad25f8693e0
Sohrab Vafa <56256479+SohrabVafa@users.noreply.github.com>
2020-12-19 13:47:27 +0100

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,25 +1,4 @@
-module.exports = (request, response) => {
-    let bodyString = request.body;
-    var entradaDialogFlow = JSON.parse(bodyString);
-    console.log("Entrada DialogFlow: "+entradaDialogFlow);
-    var pais=entradaDialogFlow['queryResult']['intent'].displayName;
-    
-    
-    var capital="Madrid";
-    
-
-    let respuesta = "La capital de __"+pais+"__ es "+capital;
-
-    // Set the status code - by default it would return 200
-    response.status = 200;
-
-    // Set the headers the way you like
-    response.headers['X-Custom-Header'] = 'CustomHeaderValue';
-    request.json().then((body) => {
-        response.send({ fulfillmentText: respuesta });
-        //return response.send({speech: "What's up cool developer :)"});
-    
-    }).catch((err) => {
-        response.send("Malformed JSON body.");
-    });
+module.exports = (req, res) => {
+  let respuesta = "La capital de __"+"__ es ";
+  res.status(200).send({ fulfillmentText: respuesta });
 };

275869e59d87b671a8fcb168014556b409f641b5
Sohrab Vafa <56256479+SohrabVafa@users.noreply.github.com>
2020-12-19 18:41:11 +0100

    Update date.js

--- api/date.js
+++ api/date.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
 //   let respuesta = "La capital de __"+"__ es ";
 //   res.status(200).send({ fulfillmentText: respuesta });
   fs = require('fs')
-  fs.readFile('datos.txt', 'utf8', function (err,data) {
+  fs.readFile('/datos.txt', 'utf8', function (err,data) {
     if (err) {
       return res.status(200).send(err);
     }
